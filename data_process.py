bf5e306dacadac36b457e3712cf35ed9990338a6
Sihan Wang <sihanwang0202@gmail.com>
2024-01-06 11:06:25 -0800

    visualize bounding boxes for the data

--- data_process.py
+++ data_process.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 class WIDERFaceDataset(Dataset):
     """
@@ -65,14 +67,66 @@ class WIDERFaceDataset(Dataset):
         boxes = self.labels[idx]
 
         if self.transform:
-            image = self.transform(image)
+            image, boxes = transform_image_and_boxes(image, boxes, self.transform)
 
-        return image, torch.tensor(boxes)
+        return image, boxes
+
+def visualize_sample(image, boxes):
+    """ Visualize an image with bounding boxes """
+    plt.imshow(image)
+    ax = plt.gca()
+    for box in boxes:
+        x, y, width, height = box
+        rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    plt.show()
+
+
+def my_collate_fn(batch):
+    images = []
+    boxes = []
+
+    for item in batch:
+        images.append(item[0])
+        boxes.append(torch.tensor(item[1]))
+
+    images = torch.stack(images, dim=0)
+    return images, boxes
+
+def transform_image_and_boxes(image, boxes, transform):
+    # Store the original size of the image
+    original_size = image.size
+
+    # Apply the transform to the image.
+    image = transform(image)
+
+    # Get the new size of the image
+    new_size = [image.shape[2], image.shape[1]]  # Tensor shape: [C, H, W]
+
+    # Scale the bounding boxes accordingly.
+    new_boxes = []
+    scale_x, scale_y = new_size[0] / original_size[0], new_size[1] / original_size[1]
+    for box in boxes:
+        x, y, width, height = box
+        new_x, new_y, new_width, new_height = x * scale_x, y * scale_y, width * scale_x, height * scale_y
+        new_boxes.append([new_x, new_y, new_width, new_height])
+
+    return image, new_boxes
 
 
 if __name__ == "__main__":
     # Dataset and DataLoader instantiation for training split
-    dataset = WIDERFaceDataset(data_root='./data', split='train')
-    print(len(dataset.img_paths), len(dataset.labels))
     transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-    data_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
+    dataset = WIDERFaceDataset(data_root='./data', split='train', transform=transform)
+    data_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4, collate_fn=my_collate_fn)
+    print(len(dataset.img_paths), len(dataset.labels))
+
+    # Get a batch of data
+    images, boxes = next(iter(data_loader))
+
+    # Convert the first image in the batch to a numpy array and scale it back to [0,255]
+    image_np = images[0].numpy().transpose((1, 2, 0))
+    image_np = (image_np * 255).astype('uint8')
+
+    # Visualize the first image in the batch along with its bounding boxes
+    visualize_sample(image_np, boxes[0])
